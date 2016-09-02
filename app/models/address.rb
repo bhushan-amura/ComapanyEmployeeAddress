a1f40c9dda958edafb3caf590a27e5428f2046db
bhushan-amura <bhushan.a@amuratech.com>
2016-09-02 12:53:25 +0530

    Update address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true 
   validates :area, presence: true
-  #belongs_to :employee,->{where("addresses.addressable_type=?","Employee")},foreign_key: :addressable_id
+  belongs_to :employee,foreign_key: :addressable_id 
+  #belongs_to :company,->{where("addresses.addressable_type=?","Company")},foreign_key: :addressable_id
   #validates_associated :addressable
 end
 
