6ab71cc2057e08e085aa202d3de18836c1287596
bhushan-amura <bhushan.a@amuratech.com>
2016-09-13 16:02:12 +0530

    Update employee model for nested attributes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,12 +1,14 @@
-class Employee < ActiveRecord::Base 
+class Employee < ActiveRecord::Base
   has_one :address, as: :addressable
   belongs_to :company
-  has_one :salary_designation 
-  validates :name, presence: true, 
+  has_one :salary_designation
+  validates :name, presence: true,
     format: { with: /\s*\w+\s*\w+/i }
   validates :email, presence: true,
     format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
     uniqueness: { case_sensitive: false }
   validates :phone, presence: true, uniqueness: true
-  validates :join_year, presence: true 
+  validates :join_year, presence: true
+  accepts_nested_attributes_for :address, allow_destroy: true
+  accepts_nested_attributes_for :salary_designation
 end
